5e1a0b4fb120fe7668006587c326a29f29989419
osbowski <osobiesam@gmail.com>
2021-10-23 10:42:56 +0200

    add-form-component

---
+++ src/components/Form.vue
@@ -0,0 +1,12 @@
+<template>
+    <form>
+        <input type="text" id="name" placeholder="Name">
+        <input type="text" id="lastname" placeholder="Last Name">
+        <input type="phone" id="phonenumber" placeholder="Phone Number">
+        <input type="email" id="email" placeholder="Email">
+        <input type="text" id="country" placeholder="Country">
+        <input type="text" id="city" placeholder="City">
+        <input type="text" id="adress" placeholder="Adress">
+
+    </form>
+</template>
