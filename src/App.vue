3ed8ebeda51f7d10a8d0ee2cce13ac66c3aec6fe
osbowski <osobiesam@gmail.com>
2021-10-23 10:42:33 +0200

    add-view-home-page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="nav">
-    <!-- <router-link to="/">Home</router-link> | -->
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
   </div>
-  <router-view/>
+  <router-view/> -->
+  <Home />
 </template>
-
-<style>
-
-</style>
+<script>
+import Home from './view/Home.vue';
+export default {
+  components:{
+    Home
+  }
+}
+</script>
+<style></style>
---
+++ src/view/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <Form />
+</template>
+
+
+<script>
+import Form from '../components/Form.vue';
+export default {
+    components:{
+        Form
+    }
+}
+</script>
