2ff3ee8fe0bb35f633c542d4fbe5a372f2099095
osbowski <osobiesam@gmail.com>
2021-10-24 09:39:21 +0200

    fetch-data-to-store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,30 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    contacts:[]
   },
   mutations: {
+    setContactsFromApi(state,payload){
+      state.contacts=[...payload];
+    }
   },
   actions: {
+    async getContactsFromApi(context){
+      try{
+        const response = await axios.get('http://test01.varid.pl:4080/api/contacts');
+        const contacts = response.data;
+       context.commit('setContactsFromApi', contacts)
+      }catch(error){
+        console.error(error);
+      }
+    }
+  },
+  getters:{
+    getContactsFromStore(state){
+      return state.contacts;
+    }
   },
   modules: {
   }
